da0dd14bef3b73b08f6b861e82bb0932432c9f01
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-06-13 23:06:27 -0700

    Update outlier.js

--- outlier.js
+++ outlier.js
@@ -1,15 +1,15 @@
-const oddNumbers = [1,2,34,54,55,34,32,11,19,17,54,66,13].filter((number) => number%2!==0);
-console.log(oddNumbers);
+// const oddNumbers = [1,2,34,54,55,34,32,11,19,17,54,66,13].filter((number) => number%2!==0);
+// console.log(oddNumbers);
 
-
+//const integers = [3,5,8];  //test this array
 
 function findOutlier(integers){
-    for (let i = 0; i < integers.length; i++){
-      const integersEven = integers.filter(integers[i] % 2 === 0);
-        if(integersEven.lenght == 1){
-        return integers[i];
-        }
-        
-        else (console.log( integers[i] % 2 != 0));
-  }
-  }
+  const integersEven = integers.filter((el) => ((el % 2) === 0));
+  const integersOdd = integers.filter((el) => ((el % 2) !== 0));
+  // console.log(integersEven);
+  // console.log(integersOdd)
+     if(integersEven.length === 1){return integersEven[0]}
+     else if(integersOdd.length === 1){return integersOdd[0]}
+}
+console.log (findOutlier([3,2,8]))  //3
+console.log(findOutlier([2,3,5]))  //2
